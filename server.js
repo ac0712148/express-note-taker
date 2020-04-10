687fa949b2ae49264cee518943c2f6aebbf02e45
Albert Cervantes <cervanalbert@gmail.com>
2020-04-09 19:01:06 -0700

    added server code to modularize routes

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require("express");
-const path = require("path");
+
+// import function for setting up routing
+const addApiRoutes = require("./routes/apiRoutes");
+const addHtmlRoutes = require("./routes/htmlRoutes");
 
 // Initialization of express server app
 const app = express();
-const PORT = 3000;
+// Port variable to to be used in local machine or use 
+//  environment port
+const PORT = process.env.PORT || 3000;
 
-// Using static
-app.use(express.static('public'));
+// Setting up the Express app to handle data parsing
+// (aka sets up req.body)
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
-// The following HTML routes should be created:
-// GET /notes - Should return the notes.html file.
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname +'/notes.html'));
-});
-// GET * - Should return the index.html file
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve('index.html'));
-})
+//These functions points the server to a series of routing files
+addApiRoutes(app);
+addHtmlRoutes(app);
 
+// The below code effectively "starts" our server
 app.listen(PORT, () => {
     console.log(`Server listening on : http://localhost:${PORT}`);
 });
