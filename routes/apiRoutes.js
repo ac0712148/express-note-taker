3a4e9b97f1c159b7dd0eab19eb321a1ff7b62869
Albert Cervantes <cervanalbert@gmail.com>
2020-04-10 19:33:06 -0700

    added delete request and edited post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,9 +16,11 @@ module.exports = (app) => {
     //  it to the db.json file, and then return the new note 
     //  to the client.
     app.post("/api/notes", (req, res) => {
-
+        const newNote = req.body;
+        // Add a unique id property to when its saved
+        newNote.id = newNote.title.replace(/\s+/g, "").toLowerCase();
         // Since data is array, push to array
-        dbData.push(req.body);
+        dbData.push(newNote);
         
         // Write file
         fs.writeFile("./db/db.json", JSON.stringify(dbData), function(error) {
@@ -31,8 +33,21 @@ module.exports = (app) => {
             // finished writing to file
             console.log("Success, Element has been added...");
         });
-        // Send response
-        res.send(true);
+        // Send response, return a new note to client
+        return res.json({});
         
+    });
+
+    // API DELETE Request
+    // Receive a query parameter containing the id of a note to delete
+    app.delete("/api/notes/:id", (req, res) => {
+      const noteIndex = dbData.findIndex(
+        (note) => note.id === req.params.id
+      );
+      if(noteIndex === -1) {
+        return res.sendStatus(404);
+      }
+      dbData.splice(noteIndex, 1);
+      return res.sendStatus(200);
     });
 };
